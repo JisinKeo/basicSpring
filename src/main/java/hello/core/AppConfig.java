9f3c9bc988ac94bac2fc6644c5797fa6b6d4e74b
JisinKeo <tntjd9514@naver.com>
2022-09-22 02:34:11 +0900

    Configuration, Bean으로 스프링 사용하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 // @Configuration 설정을 구성한다는 뜻
 // 각 메서드에 @Bean을 붙여 스프링 컨테이너에 스프링 빈으로 등록한다
-public class AppConfig { //애플리케이션의 전체 동작 방식을 구성하기 위해, 역할과 구현 클래스가 한눈에 들어온다.
 
+@Configuration
+public class AppConfig { //애플리케이션의 전체 동작 방식을 구성하기 위해, 역할과 구현 클래스가 한눈에 들어온다.
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
-
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
     public DiscountPolicy discountPolicy(){
 //       return new FixDiscountPolicy();
         return new RateDiscountPolicy();

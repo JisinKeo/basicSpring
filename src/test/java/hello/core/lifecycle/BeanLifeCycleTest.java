484a96244728525fb1ebc0cef1aed02b1068ecd7
JisinKeo <tntjd9514@naver.com>
2022-10-09 22:00:24 +0900

    프로토타입 스코프

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -16,7 +16,7 @@ public class BeanLifeCycleTest {
         ac.close();
     }
     @Configuration
-    static class LifeCycleConfig{  
+    static class LifeCycleConfig{
 
         // 스프링 빈은 객체를 생성하고, 의존관계 주입이 다 끝난 다음에야 필요한 데이터를 사용할 수 있는 준비가 완료된다.
         //스프링은 의존관계 주입이 완료되면 스프링 빈에게 콜백 메서드를 통해서 초기화 시점을 알려주는 다양한 기능을 제공
@@ -24,7 +24,7 @@ public class BeanLifeCycleTest {
         //라이프사이클 : 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
         //초기화 콜백 : 빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출
         //소멸전 콜백 : 빈이 소멸되기 직전에 호출
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");

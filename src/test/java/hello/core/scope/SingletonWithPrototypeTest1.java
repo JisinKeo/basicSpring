10d875630cef961d252d822eaf3ccb51bbaa2072
JisinKeo <tntjd9514@naver.com>
2022-10-11 17:59:51 +0900

    Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,13 +1,14 @@
 package hello.core.scope;
 
-import ch.qos.logback.core.net.server.Client;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,18 +35,19 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
-
+    // ObjectProvider : 지정한 빈을 컨테이너에서 대신 찾아주는 DL 서비스를 제공
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        @Autowired //의존성 주입
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        //ObjectFactory, ObjectProvider
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            //항상 새로운 프로토타입 빈이 생성되는 것을 확인할 수 있다.
+            //내부에서는 스프링 컨테이너를 통해 해당 빈을 찾아서 반환한다.
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;

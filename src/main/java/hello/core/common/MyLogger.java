885dcd157d90b96109525b328a7715787042a15a
JisinKeo <tntjd9514@naver.com>
2022-10-27 22:56:40 +0900

    스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,10 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// proxyMode는 껍데기만 씌워놓는 것 같은...?
+// 프록시 객체는 편리하게 request scope 사용 가능
+// 꼭 필요한 시점까지 지연처리를 한다.
 public class MyLogger {
     private String uuid;
     private String requestURL;
